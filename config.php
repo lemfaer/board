2f78dce9ab20a3a54ca1d7f64f4cc6c3399c737a
lem <lemfaer@gmail.com>
2017-03-24 21:09:00 +0200

    config

---
+++ config.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+
+	"db" => [
+		"host" => "127.0.0.1",
+		"base" => "board",
+		"user" => "aroot",
+		"pass" => "aroot",
+		"options" => [
+			PDO::MYSQL_ATTR_INIT_COMMAND => "set names utf8"
+		]
+	],
+
+	"routes" => [],
+	"access" => []
+
+];
