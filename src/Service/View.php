d0a46587a2993af15eb5bfd0f5140fdb202a4e92
lem <lemfaer@gmail.com>
2017-03-25 15:54:59 +0200

    string return value

--- src/Service/View.php
+++ src/Service/View.php
@@ -20,7 +20,7 @@ class View {
 	 * @param array $params view params
 	 * @return string generated view
 	 */
-	public function view(string $name, array $params = []) {
+	public function view(string $name, array $params = []) : string {
 		$rel = str_replace(".", DIRECTORY_SEPARATOR, $name);
 		$abs = implode(DIRECTORY_SEPARATOR, [ $this->conf["app"]["view"], $rel ]);
 		$html = "$abs.html";
