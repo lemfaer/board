9899b53bc27b13dd475ade0649d827f769733caa
lem <lemfaer@gmail.com>
2017-03-27 09:44:15 +0300

    format

--- db/simple.sql
+++ db/simple.sql
@@ -1,12 +1,12 @@
 CREATE TABLE simple_appointment (
-	id          INTEGER     NOT NULL AUTO_INCREMENT PRIMARY KEY,
-	owner_id    INTEGER     NOT NULL,
-	time_start  TIME        NOT NULL,
-	time_end    TIME        NOT NULL,
-	day         DATE        NOT NULL,
-	description TEXT        NOT NULL,
-	created     TIMESTAMP   NOT NULL DEFAULT now(),
-	updated     TIMESTAMP   NOT NULL DEFAULT now() ON UPDATE now(),
+	id          INTEGER   NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	owner_id    INTEGER   NOT NULL,
+	time_start  TIME      NOT NULL,
+	time_end    TIME      NOT NULL,
+	day         DATE      NOT NULL,
+	description TEXT      NOT NULL,
+	created     TIMESTAMP NOT NULL DEFAULT now(),
+	updated     TIMESTAMP NOT NULL DEFAULT now() ON UPDATE now(),
 	FOREIGN KEY (owner_id) REFERENCES employee(id)
 )
 
