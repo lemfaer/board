0d98cd809123c6d097406b1f52de32a924217eb6
lem <lemfaer@gmail.com>
2017-03-27 23:05:12 +0300

    appointment create

--- config.php
+++ config.php
@@ -25,6 +25,7 @@ return [
 
 	"routes" => [
 		[ "GET",  "~^testc$~", [ \Board\Controller\Test::class, "test" ] ],
+
 		[ "GET",  "~^employee$~", [ \Board\Controller\Employee::class, "list" ] ],
 		[ "GET",  "~^employee/create$~", [ \Board\Controller\Employee::class, "create" ] ],
 		[ "POST", "~^employee/create$~", [ \Board\Controller\Employee::class, "create_submit" ] ],
@@ -32,6 +33,9 @@ return [
 		[ "POST", "~^employee/update/([0-9]+)$~", [ \Board\Controller\Employee::class, "update_submit" ] ],
 		[ "GET",  "~^employee/delete/([0-9]+)$~", [ \Board\Controller\Employee::class, "delete" ] ],
 		[ "POST", "~^employee/delete/([0-9]+)$~", [ \Board\Controller\Employee::class, "delete_submit" ] ],
+
+		[ "GET",  "~^appointment/create$~", [ \Board\Controller\Appointment::class, "create" ] ],
+		[ "POST", "~^appointment/create$~", [ \Board\Controller\Appointment::class, "create_submit" ] ],
 	],
 
 	"access" => [
@@ -48,7 +52,11 @@ return [
 		"employee.pass" => "Password must contain 6-20 alphanumeric characters or symbols.",
 		"employee.created" => "Employee was successfully created",
 		"employee.updated" => "Employee was successfully updated",
-		"employee.deleted" => "Employee was successfully deleted"
+		"employee.deleted" => "Employee was successfully deleted",
+
+		"appointment.day" => "Day end must be greater than day start",
+		"appointment.time" => "Time end must be greater than time start",
+		"appointment.created" => "Appointment was successfully created"
 	],
 
 	"intervals" => [
---
+++ src/Controller/Appointment.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace Board\Controller;
+
+use DateTime;
+use Board\Model\Employee;
+use Board\Model\Appointment\Simple;
+use Board\Model\Appointment\Recurrent;
+
+class Appointment extends Controller {
+
+	/** /appointment/create */
+	public function create() {
+		$owners = Employee::all($this->connection);
+		$messages = $this->message;
+		$intervals = $this->conf["intervals"];
+		return ($this->view)("appointment.create",
+			compact("owners", "messages", "intervals"));
+	}
+
+	/** /appointment/create */
+	public function create_submit() {
+		$this->request["day"] = $this->request["day_start"];
+
+		if (!$this->validate_token() || !$this->validate_day()
+				|| !$this->validate_time()) {
+
+			return ($this->redirect)("appointment/create");
+		}
+
+		$type = filter_var($this->request["recurrent"], FILTER_VALIDATE_BOOLEAN)
+			? Recurrent::class : Simple::class;
+
+		$appointment = new $type($this->connection);
+		$appointment->from_array($this->request->getArrayCopy());
+		$appointment->save();
+
+		($this->message)("appointment.created");
+		return ($this->redirect)("/");
+	}
+
+	protected function validate_day() {
+		if (!filter_var($this->request["recurrent"], FILTER_VALIDATE_BOOLEAN)) {
+			return true;
+		}
+
+		if (isset($this->request["day_end"])) {
+			$day_start = DateTime::createFromFormat("Y-m-d", $this->request["day_start"]);
+			$day_end   = DateTime::createFromFormat("Y-m-d", $this->request["day_end"]);
+
+			if ($day_end > $day_start) {
+				return true;
+			}
+		}
+
+		($this->message)("appointment.day");
+		return false;
+	}
+
+	protected function validate_time() {
+		$time_start = DateTime::createFromFormat("H:i", $this->request["time_start"]);
+		$time_end   = DateTime::createFromFormat("H:i", $this->request["time_end"]);
+
+		if ($time_end > $time_start) {
+			return true;
+		}
+
+		($this->message)("appointment.time");
+		return false;
+	}
+
+}
